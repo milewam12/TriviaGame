0052728bd42e32d8f8f53e83046e8aaaf2a2cfe7
Milena Garcés <milenagarces@Milenas-MacBook-Pro.local>
2017-05-26 21:12:19 -0400

    progress

---
+++ assets/javascript/app.js
@@ -0,0 +1,61 @@
+$(document).ready(function () {
+
+//GLOBAL VARIABLES//
+
+var questions = {
+
+    q1: {
+        question: "Which attraction listed below is the newest in the Orlando area as of May 2001?",
+        options: ["Discovery Cove" , "Disney's Animal Kingdom" , "The Holy Land Experience" , "Islands of Adventure"],
+        answer: "The Holy Land Experience"
+    },
+
+    q2: {
+        question: "Which venue listed below is the night time entertainment area at Universal?",
+        options: ["Church Street Station","City Walk", "Pleasure Island", "Ybor City"],
+        answer: "City Walk"
+    },
+
+      q3: {
+        question: "Which city, just north of Orlando, is home to the Charles Hosmer Morse Museum of American Art and its famous collection of Tiffany Glass?",
+        options: ["Park City", "Garden City", "Winter Park", "Winter Garden"],
+        answer: "Winter Park"
+    },
+
+      q4: {
+        question: "The Disney Attraction in Orlando that houses the four Parks is?",
+        options: ["Busch Gardens" , "Universal Studios" , "Disney World" , "Gatorland"],
+        answer: "Disney World"
+    },
+
+          q5: {
+        question: "What does EPCOT Center stands for?",
+        options: ["Environmental Prototype Community of Tomorrow" , "Everymans Perfect Community of Tomorrow" , "Experimental Preview Prototype Community of Tomorrow" , "Experimental Prototype Community of Tomorrow"],
+        answer: "Experimental Prototype Community of Tomorrow"
+    },
+}
+
+
+
+
+//Time//
+var timeOut;
+var timeStarts;
+
+
+//Scores//
+var correctAnswers;
+var incorrectAnswers;
+var unanswered;
+
+// FUNCTIONS//
+
+function startTrivia(params) {
+    
+}
+
+
+
+
+//end of (document).ready
+})
